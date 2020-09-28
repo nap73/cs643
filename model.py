e7d9860401045b785d2d62ea379dd76c2ed79cec
nap73 <64916007+nap73@users.noreply.github.com>
2020-09-28 13:46:30 -0400

    Update model.py

--- model.py
+++ model.py
@@ -13,14 +13,14 @@ from pyspark.mllib.tree import RandomForest
 
 
 spark = SparkSession(sc)
-inputDF = spark.read.csv('s3://mycs643/TrainingDataset.csv',header='true', inferSchema='true', sep=';')
+inputDF = spark.read.csv('s3://bucket_name/file.csv',header='true', inferSchema='true', sep=';')
 featureColumns = [c for c in inputDF.columns if c != 'quality']
 
 
 transformed_df= inputDF.rdd.map(lambda row: LabeledPoint(row[-1], Vectors.dense(row[0:-1])))
 
 model = RandomForest.trainClassifier(transformed_df,numClasses=10,categoricalFeaturesInfo={}, numTrees=50, maxBins=64, maxDepth=20, seed=33)
-model.save(sc,"s3://mycs643/modrf.model")
+model.save(sc,"s3:bucket_name/model_created.model")
 
 
 
